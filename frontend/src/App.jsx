a69d5f0a9b8962e01c022bcb521ab88923c0b934
Tony Javiya <tony.javiya@renenergyglobal.com>
2024-10-22 19:46:56 -0700

    Added @babel/plugin-proposal-private-property-in-object to devDependencies

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,10 +14,39 @@ const App = () => {
     console.log("Headers: ", headers);
   };
 
+  const handleTemplateChange = (event) => {
+    setEmailTemplate(event.target.value);
+  };
+
   return (
     <div style={{ padding: "20px" }}>
       <h1>PAW MailMerge</h1>
       <CSVUploader onDataParsed={handleDataParsed} />
+
+      {/* Divider to show available column headers from CSV*/}
+      {headers.length > 0 && (
+        <div style={{ marginTop: "20px", border: "1px solid #ccc", padding: "10px" }}>
+          <h3>Available CSV Headers for your MailMerge Template:</h3>
+          <ul>
+            {headers.map((header, index) => (
+              <li key={index}>{`{${header}}`}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+
+      {/*Email Template */}
+      <div style={{ marginTop: "20px" }}>
+        <h3>Email Template:</h3>
+        <textarea
+          value={emailTemplate}
+          onChange={handleTemplateChange}
+          rows={5}
+          cols={50}
+          placeholder="Type your email template here..."
+          style={{ width: "100%", padding: "10px" }}
+        />
+      </div>
     </div>
   );
 };
